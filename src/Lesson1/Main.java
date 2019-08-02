0bb3937a7f838bb924f663af0ccdd5c2ce03fcbd
TrollSpace <miger2000@mail.ru>
2019-08-02 23:49:37 +0300

    Second task done.

--- src/Lesson1/Main.java
+++ src/Lesson1/Main.java
@@ -1,12 +1,29 @@
 package Lesson1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        //First task
         Integer[] data = {1, 2, 3, 4};
         change(data, 2, 3);
         for (Integer i : data) {
-            System.out.println(i + " ");
+            System.out.print(i + " ");
         }
+        System.out.println();
+
+        System.out.println(toArrayList(data));
+
+
+
+    }
+
+    //Second task
+    private static <T> List<T> toArrayList(T[] inArr){
+        return Arrays.asList(inArr);
     }
 
 
