806763f6d1e9b05c671fbbf46efc073aa103c6e7
TrollSpace <miger2000@mail.ru>
2019-08-03 02:16:46 +0300

    some errors

--- src/Lesson1/Box.java
+++ src/Lesson1/Box.java
@@ -3,20 +3,24 @@ package Lesson1;
 import java.util.ArrayList;
 
 public class Box<T extends Fruit> {
-    private ArrayList<T> fruitsInBox;
+    private ArrayList<T> fullBox;
 
     public Box() {
     }
 
     public void putFruitToBox(T fruit) {
-        fruitsInBox.add(fruit);
+        fullBox.add(fruit);
     }
 
     public float getWeight() {
         float weight = 0;
-        for (T fruit : fruitsInBox) {
+        for (T fruit : fullBox) {
             weight += fruit.getWeight();
         }
         return weight;
+    }
+
+    public boolean compare(Box box){
+        return this.getWeight() == box.getWeight();
     }
 }
