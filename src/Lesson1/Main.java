df927e331bd01876ba0a648a48af591bc051ff32
TrollSpace <miger2000@mail.ru>
2019-08-02 23:34:40 +0300

    First quest for first home work done.

--- src/Lesson1/Main.java
+++ src/Lesson1/Main.java
@@ -1,4 +1,18 @@
 package Lesson1;
 
 public class Main {
+    public static void main(String[] args) {
+        Integer[] data = {1, 2, 3, 4};
+        change(data, 2, 3);
+        for (Integer i : data) {
+            System.out.println(i + " ");
+        }
+    }
+
+
+    private static <T> void change(T[] arr, int first, int second) {
+        T arrObj = arr[first];
+        arr[first] = arr[second];
+        arr[second] = arrObj;
+    }
 }
